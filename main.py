a4ee86a28e204d6b8a5c46287fd605c64e39c170
Wojciech Mazur <odinkodilla@int.pl>
2024-06-25 18:19:41 +0200

    Add print quantity string

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ for shop in shopping:
   list_001 = shopping.get(shop)
   print('Idę do {} i kupuję tam {}.'.format(shop, list_001))
 
+print('W sumie kupuję {} produktów'.format(len(shopping['Lidl'] + shopping['Biedronka'])))
