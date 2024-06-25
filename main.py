24d3bb7843ac0bd27c13629b3da23f02ae2d0178
Wojciech Mazur <odinkodilla@int.pl>
2024-06-25 18:15:40 +0200

    Add for loop

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ shopping = {
   'Lidl'.title() : ['chleb'.title(),'bagietka'.title(),'margaryna'.title(),'kawa'.title()],
   'Biedronka'.title() : ['ziemniaki'.title(), 'mydło'.title()]
 }
+
+for shop in shopping:
+  list_001 = shopping.get(shop)
+  print('Idę do {} i kupuję tam {}.'.format(shop, list_001))
+
